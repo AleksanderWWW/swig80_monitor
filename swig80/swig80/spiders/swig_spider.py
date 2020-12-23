237640336057639f4825d5a2beced7d27992f464
AleksanderWWW <alwojnarowicz@gmail.com>
2020-12-23 15:31:50 +0100

    complete parse func

--- swig80/swig80/spiders/swig_spider.py
+++ swig80/swig80/spiders/swig_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from datetime import datetime
 
 class SwigSpider(scrapy.Spider):
     name = "swig80"
@@ -14,4 +15,24 @@ class SwigSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        table = response.css('table#fth1 tbody')
+        for row in table.css('tr'):
+            ticker = row.css('td b font a::text')[0].get()
+            name = row.css('td font#f10::text')[0].get()
+            price = row.css('td#f13  b span::text')[0].get()
+            try:
+                book_and_eps = row.css('td#f13::text').getall()
+                book_value = book_and_eps[0]
+                eps = book_and_eps[1]
+            except (IndexError, TypeError):
+                book_value, eps = 'null', 'null'
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            yield {
+                'timestamp': timestamp,
+                'ticker': ticker,
+                'name': name,
+                'price': price,
+                'book_value': book_value,
+                'eps': eps
+            }
+            
